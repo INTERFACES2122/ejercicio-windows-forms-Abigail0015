95038b0bd7b978ac4719eed06a2ca0d149824713
Abigail0015 <A298360@alumnos.uaslp.mx>
2021-08-27 21:05:37 -0500

    Agregar archivos de proyecto.

---
+++ practicaFormularios/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿
+namespace practicaFormularios
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbFirstName = new System.Windows.Forms.Label();
+            this.lbLastName = new System.Windows.Forms.Label();
+            this.lbMiddleName = new System.Windows.Forms.Label();
+            this.tbFirstName = new System.Windows.Forms.TextBox();
+            this.tbMiddleName = new System.Windows.Forms.TextBox();
+            this.tbLastName = new System.Windows.Forms.TextBox();
+            this.tbSearch = new System.Windows.Forms.TextBox();
+            this.dgInformation = new System.Windows.Forms.DataGridView();
+            this.firstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.middleName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgInformation)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(8, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Agregar  alumno";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkRed;
+            this.label2.Location = new System.Drawing.Point(8, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Buscar";
+            // 
+            // lbFirstName
+            // 
+            this.lbFirstName.AutoSize = true;
+            this.lbFirstName.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbFirstName.Location = new System.Drawing.Point(9, 43);
+            this.lbFirstName.Name = "lbFirstName";
+            this.lbFirstName.Size = new System.Drawing.Size(95, 21);
+            this.lbFirstName.TabIndex = 2;
+            this.lbFirstName.Text = "Nombre(s):";
+            // 
+            // lbLastName
+            // 
+            this.lbLastName.AutoSize = true;
+            this.lbLastName.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLastName.Location = new System.Drawing.Point(9, 107);
+            this.lbLastName.Name = "lbLastName";
+            this.lbLastName.Size = new System.Drawing.Size(150, 21);
+            this.lbLastName.TabIndex = 3;
+            this.lbLastName.Text = "Apellido Materno:";
+            // 
+            // lbMiddleName
+            // 
+            this.lbMiddleName.AutoSize = true;
+            this.lbMiddleName.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMiddleName.Location = new System.Drawing.Point(288, 43);
+            this.lbMiddleName.Name = "lbMiddleName";
+            this.lbMiddleName.Size = new System.Drawing.Size(144, 21);
+            this.lbMiddleName.TabIndex = 4;
+            this.lbMiddleName.Text = "Apellido Paterno:";
+            // 
+            // tbFirstName
+            // 
+            this.tbFirstName.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbFirstName.Location = new System.Drawing.Point(13, 67);
+            this.tbFirstName.Name = "tbFirstName";
+            this.tbFirstName.Size = new System.Drawing.Size(230, 27);
+            this.tbFirstName.TabIndex = 6;
+            this.tbFirstName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbFirstName_KeyPress);
+            // 
+            // tbMiddleName
+            // 
+            this.tbMiddleName.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbMiddleName.Location = new System.Drawing.Point(292, 67);
+            this.tbMiddleName.Name = "tbMiddleName";
+            this.tbMiddleName.Size = new System.Drawing.Size(230, 27);
+            this.tbMiddleName.TabIndex = 7;
+            this.tbMiddleName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbMiddleName_KeyPress);
+            // 
+            // tbLastName
+            // 
+            this.tbLastName.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbLastName.Location = new System.Drawing.Point(13, 131);
+            this.tbLastName.Name = "tbLastName";
+            this.tbLastName.Size = new System.Drawing.Size(230, 27);
+            this.tbLastName.TabIndex = 8;
+            this.tbLastName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbLastName_KeyPress);
+            // 
+            // tbSearch
+            // 
+            this.tbSearch.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbSearch.Location = new System.Drawing.Point(13, 191);
+            this.tbSearch.Name = "tbSearch";
+            this.tbSearch.Size = new System.Drawing.Size(509, 27);
+            this.tbSearch.TabIndex = 9;
+            this.tbSearch.TextChanged += new System.EventHandler(this.tbSearch_TextChanged);
+            // 
+            // dgInformation
+            // 
+            this.dgInformation.AllowUserToAddRows = false;
+            this.dgInformation.AllowUserToDeleteRows = false;
+            this.dgInformation.AllowUserToOrderColumns = true;
+            this.dgInformation.AllowUserToResizeColumns = false;
+            this.dgInformation.AllowUserToResizeRows = false;
+            this.dgInformation.BackgroundColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgInformation.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgInformation.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgInformation.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.firstName,
+            this.middleName,
+            this.lastName});
+            this.dgInformation.Location = new System.Drawing.Point(14, 234);
+            this.dgInformation.Name = "dgInformation";
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgInformation.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgInformation.RowHeadersVisible = false;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dgInformation.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            this.dgInformation.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dgInformation.Size = new System.Drawing.Size(508, 150);
+            this.dgInformation.TabIndex = 10;
+            // 
+            // firstName
+            // 
+            this.firstName.HeaderText = "Nombre";
+            this.firstName.Name = "firstName";
+            this.firstName.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.firstName.Width = 170;
+            // 
+            // middleName
+            // 
+            this.middleName.HeaderText = "Apellido Paterno";
+            this.middleName.Name = "middleName";
+            this.middleName.ReadOnly = true;
+            this.middleName.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.middleName.Width = 169;
+            // 
+            // lastName
+            // 
+            this.lastName.HeaderText = "Apellido Materno";
+            this.lastName.Name = "lastName";
+            this.lastName.ReadOnly = true;
+            this.lastName.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.lastName.Width = 169;
+            // 
+            // btnSave
+            // 
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnSave.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(292, 131);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(101, 20);
+            this.btnSave.TabIndex = 11;
+            this.btnSave.Text = "Guardar";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnClear.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(421, 131);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(101, 20);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "Limpiar";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(533, 402);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dgInformation);
+            this.Controls.Add(this.tbSearch);
+            this.Controls.Add(this.tbLastName);
+            this.Controls.Add(this.tbMiddleName);
+            this.Controls.Add(this.tbFirstName);
+            this.Controls.Add(this.lbMiddleName);
+            this.Controls.Add(this.lbLastName);
+            this.Controls.Add(this.lbFirstName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgInformation)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbFirstName;
+        private System.Windows.Forms.Label lbLastName;
+        private System.Windows.Forms.Label lbMiddleName;
+        private System.Windows.Forms.TextBox tbFirstName;
+        private System.Windows.Forms.TextBox tbMiddleName;
+        private System.Windows.Forms.TextBox tbLastName;
+        private System.Windows.Forms.TextBox tbSearch;
+        private System.Windows.Forms.DataGridView dgInformation;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.DataGridViewTextBoxColumn firstName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn middleName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lastName;
+    }
+}
+
---
+++ practicaFormularios/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practicaFormularios
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            validarDatos();
+        }
+
+        private void tbFirstName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13) 
+            {
+                validarDatos();
+            }
+        }
+
+        private void tbMiddleName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                validarDatos();
+            }
+        }
+
+        private void tbLastName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                validarDatos();
+            }
+        }
+
+        private void validarDatos()
+        {
+            if (tbFirstName.Text == "" || tbMiddleName.Text == "" || tbLastName.Text == "")
+            {
+                MessageBox.Show("Ningún dato puede quedar vacío", "Error en el formulario", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else 
+            {
+                dgInformation.Rows.Add(tbFirstName.Text, tbMiddleName.Text, tbLastName.Text);
+                tbFirstName.Text = "";
+                tbMiddleName.Text = "";
+                tbLastName.Text = "";
+
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            dgInformation.Rows.Clear();
+        }
+
+
+        private void tbSearch_TextChanged(object sender, EventArgs e)
+        {
+            string search = tbSearch.Text;
+            foreach(DataGridViewRow row in dgInformation.Rows)
+            {
+                for (int counter = 0; counter < dgInformation.Rows.Count; counter++)
+                {
+                    if (tbSearch.Text == "") 
+                    {
+                        dgInformation.Rows[counter].Visible = true;
+
+                    }
+                    else if (dgInformation.Rows[counter].Cells[0].Value.ToString().Contains(search))
+                    {
+                        dgInformation.Rows[counter].Visible = true;
+                    }
+                    else if (dgInformation.Rows[counter].Cells[1].Value.ToString().Contains(search))
+                    {
+                        dgInformation.Rows[counter].Visible = true;
+                    }
+                    else if (dgInformation.Rows[counter].Cells[2].Value.ToString().Contains(search)) 
+                    {
+                        dgInformation.Rows[counter].Visible = true;
+                    }
+                    else 
+                    {
+                        dgInformation.Rows[counter].Visible = false;
+                    }
+                }
+            }
+            
+        }
+
+
+
+    }
+}
